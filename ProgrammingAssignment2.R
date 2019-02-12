aa017d91b62b3357d180b51cc902ce374213cd5c
jemachli <46279975+jemachli@users.noreply.github.com>
2019-02-12 13:39:47 +0200

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,20 +1,28 @@
+## The following function creates a special "matrix" object that can cache its inverse.
+## Creation of the funcation follows the below steps:
+ 
 > makeCacheMatrix<-function(x=matrix()){ inv<-NULL set<- function(y) {x<<- y inv<<- NULL } get<- function() x setinverse <- function(inverse) inv <<-inverse getinverse <- function() inv list(set= set, get=get, setinverse=setinverse, getinverse=getinverse)}
 Error: unexpected symbol in "makeCacheMatrix<-function(x=matrix()){ inv<-NULL set"
-> makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
-  +     inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
-  +     set <- function(y) {                    ## define the set function to assign new 
-    +         x <<- y                             ## value of matrix in parent environment
-    +         inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+> makeCacheMatrix <- function(x = matrix()) {              ## define the argument with default mode of "matrix"
+  +     inv <- NULL                                        ## initialize inv as NULL; will hold value of matrix inverse 
+  +     set <- function(y) {                               ## define the set function to assign new 
+    +         x <<- y                                      ## value of matrix in parent environment
+    +         inv <<- NULL                                 ## if there is a new matrix, reset inv to NULL
     +     }
-  +     get <- function() x                     ## define the get fucntion - returns value of the matrix argument
+  +     get <- function() x                                ## define the get fucntion - returns value of the matrix argument
   +     
     +     setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
     +     getinverse <- function() inv                     ## gets the value of inv where called
-    +     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer 
-    +     ## to the functions with the $ operator
+    +     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
+                                                          ## you need this in order to refer 
+    +                                                     ## to the functions with the $ operator
       + }
+                                        
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has alreacalculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## The function assumes that the matrix supplied is always invertible.
+                                        
 > cacheSolve <- function(x, ...) {
-  +     ## Return a matrix that is the inverse of 'x'
     +     inv <- x$getinverse()
     +     if(!is.null(inv)) {
       +         message("getting cached data")
